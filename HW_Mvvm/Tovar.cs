2d5b3bf846f6fb021825163750a943e0fcb23442
Laksiya <Laksi@LAPTOP-3K4DPE8K>
2024-03-25 13:56:56 +0400

    Add project files.

---
+++ HW_Mvvm/Tovar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Mvvm
+{
+    public class Tovar : INotifyPropertyChanged
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropCh("Title");
+            }
+        }
+        private string company;
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                company = value;
+                OnPropCh("Company");
+            }
+        }
+        private int price;
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropCh("Price");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropCh([CallerMemberName] string prop_str = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop_str));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        private object selectTovar;
+
+        public object SelectTovar { get => selectTovar; set => SetProperty(ref selectTovar, value); }
+
+
+
+    }
+}
---
+++ HW_Mvvm/ViewModelTovar.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Mvvm
+{
+    public class ViewModelTovar : INotifyPropertyChanged
+    {
+        public ObservableCollection<Tovar> Tovares { get; set; }
+
+        public Tovar selectTovar;
+        public Tovar SelectTovar
+        {
+            get { return selectTovar; }
+            set
+            {
+                selectTovar = value;
+                OnPropCh("SelectTovar");
+            }
+        }
+        private void OnPropCh([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+
+        }
+
+        public ViewModelTovar()
+        {
+            Tovares = new ObservableCollection<Tovar>
+            {
+             new Tovar {Title="Молоко", Company="Ферма", Price=560 },
+             new Tovar {Title="Хлеб", Company="Пончик", Price =100},
+             new Tovar {Title="Масло", Company="Ферма", Price=300  },
+             new Tovar {Title="Мандарин", Company="Сад", Price=350}
+            };
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        My_Command add_command;
+        public My_Command ADD_command
+        {
+            get
+            {
+                return add_command ??
+                    (
+                        add_command = new My_Command(obj =>
+                        {
+                            Tovar t_new = new Tovar();
+                            Tovares.Insert(0, t_new);
+                            SelectTovar = t_new;
+                        }
+                    ));
+            }
+        }
+
+        My_Command remove_command;
+        public My_Command Remove_command
+        {
+            get
+            {
+                return remove_command ??
+                    (
+                        remove_command = new My_Command(obj =>
+                        {
+                            Tovar t = obj as Tovar;
+                            if (t != null)
+                            {
+                                Tovares.Remove(t);
+                            }
+                        },
+                        (obj) => Tovares.Count > 0)
+                    );
+            }
+        }
+
+    }
+}
